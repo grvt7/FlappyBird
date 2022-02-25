affd83e7ce659932ea7ad37eab97fbdc75cb9a6c
Garvit <r.garvit7@gmail.com>
2022-02-25 15:34:08 +0530

    Pipes Movement

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Pipes : MonoBehaviour {
+    // Refrence the speed at which the pipes move
+    public float speed = 5f;
+    // Refrences the left most edge of the game 
+    private float leftEdge;
+
+// Gets the left edge at the start of the game
+    private void Start() {
+        // Refrences the camera. And gets the position at which the left edge is.
+        // Subtracting 1f to make sure the pipes are off screen when they are destroyed.
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+    private void Update() {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        
+        if(transform.position.x < leftEdge){
+            Destroy(gameObject);
+        }
+    }
+}
